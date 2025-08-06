f4852c9963b82de7766c329a0415916593ce9e1c
DESKTOP-HEVIA\hevia <hevia.colinas.carlos@gmail.com>
2025-08-06 19:07:39 +0200

    add sample .net test

---
+++ HSoft.SampleActions.IntegrationTest/CustomersTest.cs
@@ -0,0 +1,69 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace HSoft.SampleActions.IntegrationTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<MySqlDbContext>().UseMySQL("server=localhost;database=testdb;user=root;password=root_test").Options;
+
+            using (var context = new MySqlDbContext(options))
+            {
+                context.Database.EnsureCreated();
+                var customer = new Customer
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "John",
+                    LastName = "Doe"
+                };
+                context.Customers.Add(customer);
+                context.SaveChanges();
+            }
+        }
+
+        [Category("Integration")]
+        [Test]
+        public void CustomerAdd_HappyCase()
+        {
+            var options = new DbContextOptionsBuilder<MySqlDbContext>().UseMySQL("server=localhost;database=testdb;user=root;password=root_test").Options;
+            using (var context = new MySqlDbContext(options))
+            {
+                var customers = context.Customers.ToList();
+                Assert.IsNotNull(customers);
+                Assert.That(customers.Count, Is.EqualTo(1));
+            }
+        }
+    }
+
+    public class MySqlDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        public MySqlDbContext(DbContextOptions<MySqlDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>()
+                        .HasKey(c => c.Id);
+
+            modelBuilder.Entity<Customer>().Property(c => c.Name).HasMaxLength(200);
+            modelBuilder.Entity<Customer>().Property(c => c.LastName).HasMaxLength(200);
+
+            modelBuilder.Entity<Customer>().ToTable("Customers");
+
+            // Configure your entities here
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+    public class Customer
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+    }
+}
